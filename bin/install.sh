2aa232289bd0374e617193ae3e2f60000edbc666
Ahmedul Haque Abid <a_h_abid@hotmail.com>
2025-02-08 19:01:34 +0600

    Use clone instead of download

--- bin/install.sh
+++ bin/install.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-DOWNLOAD_URL=https://github.com/a-h-abid/init-docker-nginx-php/archive/refs/heads/main.zip
+CLONE_URL=https://github.com/a-h-abid/init-docker-nginx-php.git
 TEMP_DIR_NAME=ahabid-init-docker-nginx-php-$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 6)
 
-mkdir -p /tmp/$TEMP_DIR_NAME
-curl -L -o /tmp/$TEMP_DIR_NAME/repo.zip $DOWNLOAD_URL
-unzip /tmp/$TEMP_DIR_NAME/repo.zip -d /tmp/$TEMP_DIR_NAME
+git clone $CLONE_URL /tmp/$TEMP_DIR_NAME
 sh /tmp/$TEMP_DIR_NAME/init-docker-nginx-php-main/bin/init-docker-nginx-php
 
 rm -rf /tmp/$TEMP_DIR_NAME
