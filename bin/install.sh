194ea0e9b0125dfe0703d391e96b2fc07ee6b014
Ahmedul Haque Abid <a_h_abid@hotmail.com>
2025-02-24 23:19:42 +0600

    fix: wrong path to command

--- bin/install.sh
+++ bin/install.sh
@@ -4,6 +4,6 @@ CLONE_URL=https://github.com/a-h-abid/init-docker-nginx-php.git
 TEMP_DIR_NAME=ahabid-init-docker-nginx-php-$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 6)
 
 git clone $CLONE_URL /tmp/$TEMP_DIR_NAME
-sh /tmp/$TEMP_DIR_NAME/init-docker-nginx-php-main/bin/init-docker-nginx-php
+sh /tmp/$TEMP_DIR_NAME/bin/init-docker-nginx-php
 
 rm -rf /tmp/$TEMP_DIR_NAME
